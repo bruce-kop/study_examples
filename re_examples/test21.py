433b809e9da9262036a4374f8cb3e86e4a2ded02
bruce-kop <410266441@qq.com>
2022-09-17 17:07:16 +0800

    删除一个错误的装饰器函数。
    这个错误的装饰器函数返回的是一个浮点型，不是一个函数指针，所以我们不需要这样的函数

--- re_examples/test21.py
+++ re_examples/test21.py
@@ -9,17 +9,7 @@ def deco(func):
         print("time is %d ms"%msecs)
     return func
 
-
-def warpper(func):
-    startTime = time.time()
-    ret = func()
-    endTime = time.time()
-
-    msecs = (endTime-startTime)*1000
-    print("time is %d ms"%msecs)
-    return msecs
-
-@warpper
+@deco
 def func():
     print("hello")
     time.sleep(1)
